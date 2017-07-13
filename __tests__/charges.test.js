cedafc2bf950aa22d364eabee1943faae3c444ff
Martin Wheeler <martin@martinwheeler.com.au>
2017-07-13 20:47:44 +1000

    Add charge with customer token

--- __tests__/charges.test.js
+++ __tests__/charges.test.js
@@ -9,104 +9,82 @@ const api = new PinAPI({
   live
 })
 
+const expectedCharge = {
+  "amount": 6000,
+  "amount_refunded": 0,
+  "authorisation_expired": false,
+  "captured": true,
+  "card": {
+    "address_city": "Brisbane",
+    "address_country": "AU",
+    "address_line1": "100 Edward Street",
+    "address_line2": null,
+    "address_postcode": "4000",
+    "address_state": "QLD",
+    "customer_token": null,
+    "display_number": "XXXX-XXXX-XXXX-0000",
+    "expiry_month": 12,
+    "expiry_year": 2018,
+    "name": "Mr Robot",
+    "primary": null,
+    "scheme": "master",
+    "token": expect.any(String)
+  },
+  "created_at": expect.any(String),
+  "currency": "AUD",
+  "description": "400 fidget spinners",
+  "email": "roland@pin.net.au",
+  "error_message": null,
+  "ip_address": testIPAddress,
+  "merchant_entitlement": 5865,
+  "metadata": {},
+  "refund_pending": false,
+  "settlement_currency": "AUD",
+  "status_message": "Success",
+  "success": true,
+  "token": expect.any(String),
+  "total_fees": 135,
+  "transfer": []
+}
+
 describe('Charges Endpoints', () => {
   test('creating a charge with a complete card object', () => {
     const actual = api.createCharge(testChargeWithCard)
-    const expected = {
-      "amount": 6000,
-      "amount_refunded": 0,
-      "authorisation_expired": false,
-      "captured": true,
-      "card": {
-        "address_city": "Brisbane",
-        "address_country": "AU",
-        "address_line1": "100 Edward Street",
-        "address_line2": null,
-        "address_postcode": "4000",
-        "address_state": "QLD",
-        "customer_token": null,
-        "display_number": "XXXX-XXXX-XXXX-0000",
-        "expiry_month": 12,
-        "expiry_year": 2018,
-        "name": "Mr Robot",
-        "primary": null,
-        "scheme": "master",
-        "token": expect.any(String)
-      },
-      "created_at": expect.any(String),
-      "currency": "AUD",
-      "description": "400 fidget spinners",
-      "email": "roland@pin.net.au",
-      "error_message": null,
-      "ip_address": testIPAddress,
-      "merchant_entitlement": 5865,
-      "metadata": {},
-      "refund_pending": false,
-      "settlement_currency": "AUD",
-      "status_message": "Success",
-      "success": true,
-      "token": expect.any(String),
-      "total_fees": 135,
-      "transfer": []
-    }
 
     expect.assertions(1)
 
     return actual.then((response) => {
-      expect(response).toEqual(expected)
+      expect(response).toEqual(expectedCharge)
     })
   })
 
   test('create a charge with a card token', () => {
     const actual = api.createCharge
-    const expected = {
-      "amount": 6000,
-      "amount_refunded": 0,
-      "authorisation_expired": false,
-      "captured": true,
-      "card": {
-        "address_city": "Brisbane",
-        "address_country": "AU",
-        "address_line1": "100 Edward Street",
-        "address_line2": null,
-        "address_postcode": "4000",
-        "address_state": "QLD",
-        "customer_token": null,
-        "display_number": "XXXX-XXXX-XXXX-0000",
-        "expiry_month": 12,
-        "expiry_year": 2018,
-        "name": "Mr Robot",
-        "primary": null,
-        "scheme": "master",
-        "token": expect.any(String)
-      },
-      "created_at": expect.any(String),
-      "currency": "AUD",
-      "description": "400 fidget spinners",
-      "email": "roland@pin.net.au",
-      "error_message": null,
-      "ip_address": testIPAddress,
-      "merchant_entitlement": 5865,
-      "metadata": {},
-      "refund_pending": false,
-      "settlement_currency": "AUD",
-      "status_message": "Success",
-      "success": true,
-      "token": expect.any(String),
-      "total_fees": 135,
-      "transfer": []
-    }
 
     expect.assertions(1)
 
     return api.createCard(testCard)
       .then(cardResponse =>
-        actual(
-          {
-            ...testCharge,
-            card_token: cardResponse.token
-          }
-        ).then(chargeResponse => expect(chargeResponse).toEqual(expected))
+        actual({
+          ...testCharge,
+          card_token: cardResponse.token
+        }).then(chargeResponse => expect(chargeResponse).toEqual(expectedCharge))
+      )
+  })
+
+  test('create charge with a customers token', () => {
+    const actual = api.createCharge
+    const { card } = expectedCharge
+    const expectedChargeWithCustomer = { ...expectedCharge, card: { ...card, customer_token: expect.any(String), primary: true } }
+
+    expect.assertions(1)
+
+    return api.createCustomer(testCustomer)
+      .then(customerResponse => 
+        actual({
+          ...testCharge,
+          customer_token: customerResponse.token
+        }).then(chargeResponse => expect(chargeResponse).toEqual(expectedChargeWithCustomer))
       )
   })
 })
